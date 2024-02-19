079dfc16284f8090685355b33d8476c34558d423
kenjitheman <btwdesu@gmail.com>
2024-02-19 22:35:32 +0200

    chore: init project structure

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wezer',
+    version='0.1.0',
+    packages=find_packages('wezer'),
+    package_dir={'': 'wezer'},
+    entry_points={
+        'console_scripts': [
+            'wezer = wezer.cli:main',
+        ],
+    },
+    install_requires=[
+        'requests',
+        'click',
+        'colorama',
+        'asciichart',
+        'python-dotenv',
+    ],
+    author='kenjitheman',
+    author_email='btwdesu@gmail.com',
+    author_github='github.com/kenjitheman',
+    description='CLI Weather App with colors and data visualization',
+    url='https://github.com/kenjitheman/wezer',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
